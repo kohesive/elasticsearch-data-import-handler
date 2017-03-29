d11c91d2c296b07c0757ebf093ee651da61a613f
apatrida <jayson.minard@gmail.com>
2017-03-29 16:50:11 -0300

    go back to original test case for manual test

--- src/test/kotlin/uy/kohesive/elasticsearch/dataimport/ManualTestOfDataImport.kt
+++ src/test/kotlin/uy/kohesive/elasticsearch/dataimport/ManualTestOfDataImport.kt
@@ -7,7 +7,7 @@ class ManualTestOfDataImport {
     // TODO: This test requires Elasticsearch to be available, it is difficult to Run ES and Spark together due to conflicting dependencies
     companion object {
         @JvmStatic fun main(args: Array<String>) {
-            App().run(File("/Users/jminard/DEV/Other/Andrew-Albrecht/originals/andrew-es-data-import.conf").inputStream())
+            App().run(ClassLoader.getSystemResourceAsStream("manual-test.conf"))
         }
     }
 }
